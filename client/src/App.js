02eaf01ea44b6257a4be9c9afced702d4e36a611
kitch-finn <fin@kakao.com>
2022-05-26 14:20:51 +0900

    Revert "Feat: Add dummy login axios"
    This reverts commit 768069529395e1cf77d0f029676840f31cff241b.

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { } from 'react';
-import { BrowserRouter, Route, Switch, } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Nav from './components/Nav';
 import Footer from './components/Footer';
@@ -8,9 +8,6 @@ import Map from './pages/Map';
 import Test from './pages/Test/Test';
 import TestResults from './pages/Test/TestResults';
 import Login from './components/Sign/Login'
-import Mypage from './pages/Mypage'
-// import LoginDummy from './pages/Mypage'
-import Signup from './components/Sign/Signup'
 
 function App() {
 
@@ -23,10 +20,7 @@ function App() {
             <Route path="/map" component={ Map } />
             <Route path="/test" component={ Test } />
             <Route path="/results" component={ TestResults } />
-
             <Route path="/login" component={ Login } />
-            <Route path="/mypage" component={ Mypage } />
-            <Route path="/signup" component={ Signup } />
           </Switch>
 
         <Footer />
--- client/src/components/Sign/Login.js
+++ client/src/components/Sign/Login.js
@@ -1,42 +1,10 @@
-import React, { useState } from 'react';
+import React, {  } from 'react';
 
-
-const axios = require("axios");
 
 function Login() {
-  const [loginInfo, setLoginInfo] = useState({
-    email: '',
-    password: ''
-  });
-
-  const handleInputValue = (key) => (e) => {
-    setLoginInfo({ ...loginInfo, [key]: e.target.value });
-  };
-
-  const handleLogin = () => {
-    const { email, password } = loginInfo;
-    
-    axios.post(
-      "http://localhost:3000/users/login",
-      { email: email, password: password},
-      {
-        headers: { 'Content-Type': 'application/json'},
-        withCredentials: true,
-      }).catch(err => console.log(err))
-    }
-  
 
   return (
-    <>    
-      <div>email
-        <input onChange={handleInputValue('email')}></input>
-      </div>
-      <div>password
-        <input onChange={handleInputValue('password')}></input>
-      </div>
-      <button onClick={handleLogin}>login</button>
-    </>
-
+    <div>Login</div>
   );
 }
 
