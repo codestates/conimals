6e6850ca10b5f3817d9ae2f0cc1834b0fd3c33d3
SEB_Full_37_정민규 <jmingyu92@gmail.com>
2022-06-02 11:36:38 +0900

    Fix

--- client/src/pages/Map.js
+++ client/src/pages/Map.js
@@ -1,11 +1,7 @@
-import React, {  } from 'react';
-
+import React from 'react';
 
 function Map() {
-
-  return (
-    <div>Map</div>
-  );
+  return <div>ConimalsMap</div>;
 }
 
 export default Map;
