5984aea90c97979bcc96e23e1c46f1960acc990f
SEB_Full_37_정민규 <jmingyu92@gmail.com>
2022-06-06 13:43:15 +0900

    Feat: save new post

---
+++ client/src/pages/post/EditPost.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const EditPost = () => {
+  return (
+    <>
+      <div>edit page</div>
+    </>
+  );
+};
+
+export default EditPost;
