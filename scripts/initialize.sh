e393f16104d6c7b9d649faca8e232ac6ace2c9cb
YeonSeoJo <91008736+YeonSeoJo@users.noreply.github.com>
2022-06-14 14:38:10 +0900

    Create initialize.sh

---
+++ scripts/initialize.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cd /home/ubuntu/conimals/server
+sudo npm install -g n
+sudo n 17.5.0
+npm install pm2@latest -g
+sudo apt-get update
+sudo apt-get install authbind
+sudo touch /etc/authbind/byport/80
+sudo chown ubuntu /etc/authbind/byport/80
+sudo chmod 755 /etc/authbind/byport/80
+
