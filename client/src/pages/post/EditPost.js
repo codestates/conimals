669ad605e8a825d9bf6c387a2ab1afdf61a778f2
hsly22xk <hsly22xk@naver.com>
2022-06-16 20:55:59 +0900

    Feat: client/EditPost.js 주석 코드 추가

--- client/src/pages/post/EditPost.js
+++ client/src/pages/post/EditPost.js
@@ -225,3 +225,123 @@ const EditPost = () => {
 };
 
 export default EditPost;
+
+// const EditPost = () => {
+//   const [edited, setEdited] = useState([]);
+//   const [loading, setLoading] = useState(false);
+//   const [modalMsg, setModalMsg] = useState('');
+//   const [modalOpen, setModalOpen] = useState(false);
+
+//   const { id } = useParams();
+
+//   const modalHandler = () => {
+//     setModalOpen(false);
+//   };
+
+//   useEffect(async () => {
+//     const json = await (
+//       await fetch(`${process.env.REACT_APP_API_URL}/posts/view/${id}`)
+//     ).json();
+//     setEdited(json.data[0]);
+//   }, []);
+
+//   const onEditChange = (e) => {
+//     setEdited({
+//       [e.target.name]: e.target.value,
+//     });
+//   };
+
+//   const onSubmit = async () => {
+//     setLoading(true);
+//     try {
+//       await axios
+//         .patch(
+//           `${process.env.REACT_APP_API_URL}/posts/edit/${edited.id}`,
+//           {
+//             title: edited.title,
+//             content: edited.content,
+//             image: edited.image,
+//           },
+//           {
+//             withCredentials: true,
+//           }
+//         )
+//         .json()
+//         .then(() => {
+//           setModalOpen(true);
+//           setModalMsg('수정 완료!');
+//           setLoading(false);
+//           Navigate('/posts/board');
+//         });
+//       // .then((res) => {
+//       //   axios.post(
+//       //     `${process.env.REACT_APP_API_URL}/posts/view/${edited.id}`,
+//       //     { ...res.data.data },
+//       //     {
+//       //       headers: {
+//       //         authorization: `Bearer ${localStorage.getItem('user')}`,
+//       //       },
+//       //       withCredentials: true,
+//       //     }
+//       //   );
+//       // })
+//     } catch (err) {
+//       console.log(err);
+//     }
+//   };
+
+//   const imageUrl = `${process.env.REACT_APP_API_URL}/posts/${edited.image}`;
+
+//   return (
+//     <>
+//       {loading ? <Loading /> : null}
+//       <EditSection className='editedSection'>
+//         <EditWrap>
+//           <TextField
+//             id='standard-basic'
+//             variant='standard'
+//             fullWidth
+//             margin='normal'
+//             inputProps={{ style: { fontSize: 34 } }}
+//             placeholder='제목을 입력하세요'
+//             type='text'
+//             maxLength={70}
+//             value={edited.title}
+//             onChange={onEditChange}
+//           ></TextField>
+
+//           {imageUrl ? (
+//             <img
+//               src={imageUrl}
+//               alt='uploaded-img'
+//               style={{ width: '200px' }}
+//             ></img>
+//           ) : (
+//             ''
+//           )}
+
+//           <TextField
+//             id='outlined-basic'
+//             variant='outlined'
+//             fullWidth
+//             multiline={true}
+//             rows={8}
+//             margin='normal'
+//             placeholder='간단히 설명해주세요 (최대 500자)'
+//             maxLength={500}
+//             value={edited.content}
+//             onChange={onEditChange}
+//           ></TextField>
+//           <Button variant='contained' type='button' onClick={onSubmit}>
+//             수정
+//           </Button>
+//         </EditWrap>
+//       </EditSection>
+//       {modalOpen ? (
+//         <ConfirmModal handleModal={modalHandler}>{modalMsg}</ConfirmModal>
+//       ) : null}
+//     </>
+//   );
+// };
+
+// export default EditPost;
