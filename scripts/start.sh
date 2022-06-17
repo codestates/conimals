5b0d011e90d2c6fc1b77cd63ba47434186ee6a78
YeonSeoJo <irwinjoebeck@gmail.com>
2022-06-17 20:16:31 +0900

    fix: fix env and path

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /home/ubuntu/im-sprint-practice-deploy/server
+cd /home/ubuntu/conimals/server
 
 export DATABASE_USER=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_USER --query Parameters[0].Value | sed 's/"//g')
 export DATABASE_PASSWORD=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_PASSWORD --query Parameters[0].Value | sed 's/"//g')
